8a3288ccbbe456b2db3cbc7d6539476e77d1754f
OAkira Pei <ouchangpei0321@outlook.com>
2026-04-06 21:23:16 +0800

    created home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,33 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+    <main className="min-h-screen bg-black responsive-padding-1">
+      <section className="relative grid-system">
+        {/* Title */}
+        <header className="mb-6 text-white">
+          <h1 className="absolute left-0 bottom-full page-title-font text-6xl">
+            Ouchngpui
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+        </header>
+        {/* Main Grid */}
+        <div className="bg-(--background-grey-1) col-start-3 col-span-2 row-start-1 row-span-2 flex items-center justify-center"></div>
+
+        <div className="bg-(--background-writing) col-start-5 col-span-1 row-start-1 row-span-1 flex items-center justify-center"></div>
+        <div className="bg-(--background-research) col-start-6 col-span-1 row-start-1 row-span-1 flex items-center justify-center"></div>
+        <div className="bg-(--background-photos) col-start-5 col-span-1 row-start-2 row-span-1 flex items-center justify-center"></div>
+        <div className="bg-(--background-design) col-start-6 col-span-1 row-start-2 row-span-1 flex items-center justify-center"></div>
+
+        <div className="text-[#D0D0D0] leading-2.5 text-[8px] col-start-3 col-span-2 row-start-3 row-span-1">
+          <p>
+            The new epoch calls for a more humane society, one that defies the
+            mechanization of corporal nature. New human beings search for static
+            contemplation and the fleshly sensation of dynamics. We must
+            establish the unity between understanding the world and free
+            self-expression through every possible form.
+            &quot;Equality.&quot; Someone spoke the word. We must be better through
+            creation. That’s the only answer.
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+      </section>
+    </main>
   );
 }
